7664fd91e1425555594537439ff0da3569b509de
coatsnmore <coats.18@gmail.com>
2018-04-28 12:34:08 -0400

    moved convention to mount /home/src in container

--- run-workspace.cmd
+++ run-workspace.cmd
@@ -1,4 +1,4 @@
 SET TAG=coatsn/alpine-workspace
 SET USER_WORKSPACE=//c//devl//docker-workspace//test
 REM docker run --mount type=volume,src=//c//devl//docker-workspace//test,dst=/data -it %TAG%
-docker run -v %USER_WORKSPACE%:/data/src -it %TAG%
+docker run -v %USER_WORKSPACE%:/home/src -it %TAG%
